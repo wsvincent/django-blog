01d324ad9b9c32608aadb78a58d0bbb1a9025a9b
Will Vincent <will@wsvincent.com>
2026-04-16 11:12:31 -0400

    Initial commit

---
+++ posts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import PostListView, PostDetailView
+
+urlpatterns = [
+    path("<int:pk>/", PostDetailView.as_view(), name="post_detail"),
+    path("", PostListView.as_view(), name="post_list"),
+]
